7560123a985b3f60b9b9bc6c3dc0f6243be2c91e
Mohit Shah <mohitshah7777@gmail.com>
2021-05-26 18:20:35 +0530

    Added: Ability to reset the form on clicking reset

--- js/EmployeePayroll.js
+++ js/EmployeePayroll.js
@@ -18,37 +18,44 @@ class EmployeePayrollData{
     get profilepic(){ return this._profilepic; }
     set profilepic(profilepic){
         this._profilepic=profilepic;
+        console.log(this._profilepic);  
     }
 
     get gender(){ return this._gender; }
     set gender(gender){
         this._gender=gender;
+        console.log(this._gender);
     }
     
     get department(){ return this._department; }
     set department(department){
         this._department=department;
+        console.log(this._department);
     }
     
     get salary(){ return this._salary; }
     set salary(salary){
         this._salary=salary;
+        console.log(this._salary);
     }
 
     get note(){ return this._note; }
     set note(note){
         this._note=note;
+        console.log(this._note);
+    
     }
 
     get startDate(){ return this._startDate; }
     set startDate(startDate){
         this._startDate=startDate;
+        console.log(this._startDate);
     }
 
     toString(){
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
         const empDate = !this.startDate ? "undefined" :
-                        this.startDate.toLocaleeDateString("en-us", options);
+                        this.startDate.toLocaleDateString("en-us", options);
         return "id=" +this.id +", name='"+this.name + ", gender='" + this.gender +
                     ", profilepic='" + this.profilepic + ", department='" + this.department +
                     ", salary='" + this.salary + ", startDate='" + empDate + ", note='" + this.note; 
